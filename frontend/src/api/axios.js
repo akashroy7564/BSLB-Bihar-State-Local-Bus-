d50bf7f22f0a506a01bee3ad795f166e0f099585
akashroy7564 <hiakashkumar123@gmail.com>
2026-08-08 14:39:30 +0530

    .env added

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,11 @@
 import axios, { Axios } from "axios";
 
-const api= axios.create({
-    baseURL:"https://bslb-bihar-state-local-bus.onrender.com/api"
-})
+// const api= axios.create({
+//     baseURL:"https://bslb-bihar-state-local-bus.onrender.com/api"
+// })
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+});
 
 export default api;
